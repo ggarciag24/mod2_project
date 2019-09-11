4ae8ee6de0c154263fee128a5f5072c9e1ec9584
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-11 14:13:45 -0400

    Account option created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :teams
   resources :players, only: [:index, :show]
+  resources :users, only: [:create]
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   delete "/logout", to: 'sessions#destroy'
+  get "/account", to: "users#new"
 end
