5ae4338be83bb9798d5137b27b73210a903454da
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-11 11:04:21 -0400

    edit works

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -30,8 +30,34 @@ end
 
 def update
   team = Team.find(params[:id])
+  # team.update(team_params)
 
-  team.update(team_params)
+  pre_edit_p = TeamPlayer.select do |indiv_tp|
+    indiv_tp.team_id == team.id
+  end
+  pre_edit_p.each do |play|
+    play.destroy
+  end
+  # Very close! The general idea is correct, but you have to test this code
+  # What are you assuming the return is of .find_by? What does it actually return?
+  ## returns the first connection
+  # Does that return have a .destroy_all method?
+  ## only one thing so no
+  # (Be thorough in checking you assumptions,
+  #  and when a check doesn't make sense,
+  #  ask a coach to explain it)
+
+
+  name = params[:team][:name]
+  city = params[:team][:city]
+  mascot = params[:team][:mascot]
+  point_guard = params[:team][:point_guard_id]
+  shooting_guard = params[:team][:shooting_guard_id]
+  small_forward = params[:team][:small_forward_id]
+  power_forward = params[:team][:power_forward_id]
+  center = params[:team][:center_id]
+
+  team.update(name: name, city: city, mascot: mascot, point_guard_id: point_guard, shooting_guard_id: shooting_guard, small_forward_id: small_forward, power_forward_id: power_forward, center_id: center )
 
   redirect_to team_path(team)
 end
@@ -45,15 +71,9 @@ end
 
 private
   def team_params
-<<<<<<< HEAD
-    params.require(:team).permit(:name, :city, :mascot, player_attributes: [:name, :jersey_number, :position, :points,
-       :assists, :rebounds, :FGP, :current_team])
-=======
     params.require(:team).permit(:name, :city, :mascot, :point_guard_id,:shooting_guard_id, :small_forward_id, :power_forward_id, :center_id,
       players_attributes: [:name, :jersey_number, :position, :points, :assists,
          :rebounds, :current_team, :FGP])
-
->>>>>>> 95cf14141ebade5debb5806e54ea0b66a3b3ea4b
   end
 
 end
