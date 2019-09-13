e6063793f87bb386140c7785665f1b9fa1b8f992
github username <github email address>
2019-09-13 11:52:10 -0400

    new CSS stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def create
       @user = User.new(user_params)
-
       if @user.valid?
         @user.save
         session[:username] = @user.username
